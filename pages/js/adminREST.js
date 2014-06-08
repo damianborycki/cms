15ca98e7af9a6f69f838b3fc804998bcbfca8433
Maciek <0my0key0@gmail.com>
2014-06-08 21:04:27 +0200

    Zmiany w adminie

--- pages/js/adminREST.js
+++ pages/js/adminREST.js
@@ -4,3 +4,17 @@ $http({method: 'GET', url: '/portal/tag'}).
 		$scope.listOfTags = data;
 	  }); 
 	};
+	
+function GetTagTypes($scope, $http){
+$http({method: 'GET', url: '/portal/type'}).
+	  success(function(data, status, headers, config) {
+		$scope.listOfTagsTypes = data;
+	  }); 
+	};
+	
+function GetCategories($scope, $http){
+$http({method: 'GET', url: '/portal/category'}).
+	  success(function(data, status, headers, config) {
+		$scope.listOfCategories = data;
+	  }); 
+	};
