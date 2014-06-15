9c8fe8985800a50d36a32ea2c9be1f969327f0e4
Konrad Winiarski <winek353@gmail.com>
2014-06-15 16:29:01 +0200

    implementacja pobieranie niezatwierdzonych obrazkow

--- Portal/src/main/java/com/portal/controller/ImageController.java
+++ Portal/src/main/java/com/portal/controller/ImageController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile; 
 import java.io.File;
 import java.io.FileWriter;
@@ -18,6 +19,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.*;
+import java.util.List;
 
 import magick.*;
 
@@ -83,11 +85,17 @@ public class ImageController {
     public void getImage(HttpServletResponse response,
                          @RequestParam("id") String imageId) throws Exception
     {
-        //File file = new File(getDefaultImageLink() );  // getImagePath(imageId));
         File file = new File( getImagePath(imageId));
         FileInputStream is = new FileInputStream(file);
         IOUtils.copy(is, response.getOutputStream());
         response.flushBuffer();
+    }
+
+    @RequestMapping(value="/unapproved_images", method=RequestMethod.GET)
+    @ResponseBody
+    public List<Image> getUnapprovedImages()
+    {
+        return imageDAO.getAllUnapproved();
     }
 
     private String getNewImagePath(MultipartFile file)
--- Portal/src/main/java/com/portal/dao/interfaces/ImageDAOI.java
+++ Portal/src/main/java/com/portal/dao/interfaces/ImageDAOI.java
@@ -1,10 +1,12 @@
 package com.portal.dao.interfaces;
 
 import com.portal.entity.Image;
+import java.util.List;
 
 public interface ImageDAOI {
 
     public Image getImage(String id);
     public void addImage(Image i);
+    public List<Image> getAllUnapproved();
 
 }
